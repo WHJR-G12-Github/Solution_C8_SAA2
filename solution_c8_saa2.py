79f8be55a196b049df331ff7cc93c8daf7bc97ed
WHJR-Grade12 <91465322+WHJR-G12-Github@users.noreply.github.com>
2021-10-28 20:30:36 +0530

    Create solution_c8_saa2.py

---
+++ solution_c8_saa2.py
@@ -0,0 +1,11 @@
+# Defining a function 'power()' with 'a', 'b' as parameters
+def power(a,b):
+    # Calculating 'result' using power operator
+    result = a**b
+    # Returning the 'result'
+    return result
+
+  
+# Calling the function 'power()' with '2' and '3' as arguments 
+# and printing the result
+print(power(2,3))
